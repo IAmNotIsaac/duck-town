98216a33c69dd3fc474f8e6f54fc045891ac305f
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-05-30 14:15:23 -0700

    added lever animation

--- Scripts/Switch.cs
+++ Scripts/Switch.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Switch : MonoBehaviour
 {
     [SerializeField] private DoorManager _door;
+    [SerializeField] private Animator _animator;
 
     public void Interact()
     {
         _door.Open();
+        _animator.Play("Base Layer.Activate");
     }
 }
