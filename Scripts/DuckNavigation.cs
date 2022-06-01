7bf57d53799f41fb482137aacb6ef3b34045c421
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-05-31 22:30:41 -0700

    duck ai improvements

--- Scripts/DuckNavigation.cs
+++ Scripts/DuckNavigation.cs
@@ -10,6 +10,7 @@ public class DuckNavigation : MonoBehaviour
     public enum NavState {
         WANDER,             // go to random location
         CHASE,              // go to player's last seen location
+        CHECK,              // we lost player, look around area
         ALERT,              // go to alert location
         EXIT                // go to level exit
     }
@@ -18,9 +19,11 @@ public class DuckNavigation : MonoBehaviour
 
     // [SerializeField] public Vector3 _destination;
     [SerializeField] private NavMeshAgent _agent;
-    [SerializeField] private Transform _modelTransform;
+    [SerializeField] private PlayerController _player;
     [SerializeField] private Transform[] _wanderPoints;
     [SerializeField] private Transform _exitPoint;
+    [SerializeField] private Transform _modelTransform;
+    [SerializeField] private Transform _eyesTransform;
 
     private NavState _navState = NavState.WANDER;
 
@@ -31,12 +34,13 @@ public class DuckNavigation : MonoBehaviour
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(_navState);
         StateUpdate(_navState);
         FixFacingDirection();
     }
@@ -46,11 +50,12 @@ public class DuckNavigation : MonoBehaviour
     {
         _modelTransform.localEulerAngles = new Vector3(
             _modelTransform.localEulerAngles.x,
-            Mathf.LerpAngle(_modelTransform.localEulerAngles.y, _targetAngle + 90.0f, 0.1f),
+            _targetAngle + 90.0f,
+            // Mathf.LerpAngle(_modelTransform.localEulerAngles.y, _targetAngle + 90.0f, 0.9f),
             _modelTransform.localEulerAngles.z
         );
 
-        _targetAngle = Vector3.Angle(transform.position, transform.position + _agent.velocity);
+        _targetAngle = Vector3.Angle(Vector3.zero, _agent.velocity);
     }
 
 
@@ -58,6 +63,21 @@ public class DuckNavigation : MonoBehaviour
     {
         switch (_navState) {
             case NavState.WANDER: {
+                RaycastHit hit;
+                if (Physics.Linecast(_eyesTransform.position, _player.transform.position, out hit))
+                {
+                    if (hit.collider.GetComponent<PlayerController>())
+                    {
+                        // TODO: potential angle check?
+                        // ^^ currently it works as the line collides with the duck's body, though
+                        // ^^ something to look into and consider.
+
+                        SwitchState(NavState.CHASE);
+                    }
+                }
+
+
+                // Go to random wander points
                 if (Vector3.Distance(_agent.destination, transform.position) < 1.0f)
                 {
                     _idleTime += Time.deltaTime;
@@ -65,6 +85,7 @@ public class DuckNavigation : MonoBehaviour
                     if (_idleTime >= IDLE_WANDER_TIME)
                     {
                         _agent.destination = _wanderPoints[_rnd.Next() % _wanderPoints.Length].position;
+                        _idleTime = 0.0f;
                     }
                 }
 
@@ -72,6 +93,27 @@ public class DuckNavigation : MonoBehaviour
             }
 
             case NavState.CHASE: {
+                // We only want the duck to go to the player's last known location
+                RaycastHit hit;
+                if (Physics.Linecast(_eyesTransform.position, _player.transform.position, out hit) && hit.collider.GetComponent<PlayerController>())
+                {
+                    _agent.destination = _player.transform.position;
+                }
+
+                // Player is not in view
+                else
+                {
+                    // We're nearby our destination, but we don't see the player.
+                    if (Vector3.Distance(_agent.destination, transform.position) < 1.0f)
+                    {
+                        SwitchState(NavState.WANDER);
+                    }
+                }
+
+                break;
+            }
+
+            case NavState.CHECK: {
                 break;
             }
 
@@ -89,6 +131,7 @@ public class DuckNavigation : MonoBehaviour
 
     void LoadState(NavState newState)
     {
+        Debug.Log(newState);
         switch (newState)
         {
             case NavState.WANDER: {
