b3f12cdc21d29d61cf8e1a6ff13308956a6df8a3
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-06-04 18:21:05 -0700

    ambience doesnt play in final level now

--- Scripts/Ambience.cs
+++ Scripts/Ambience.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Ambience : MonoBehaviour
 {
     const float MUSIC_INTERMEDIATE_TIME = 30.0f;
     const float SCREAMS_INTERMEDIATE_TIME = 300.0f;
+    const int FINAL_LEVEL_ID = 5;
 
     [SerializeField] private AudioSource[] _music;
     [SerializeField] private AudioSource[] _screams;
@@ -14,31 +16,47 @@ public class Ambience : MonoBehaviour
     private float _timeSinceScream = 150.0f;
     private AudioSource _currentMusic;
     private System.Random _rnd = new System.Random();
+    private bool _finalLevel = false;
 
 
     void Awake()
     {
+        SceneManager.sceneLoaded += OnSceneLoaded;
         DontDestroyOnLoad(gameObject);
+    }
+
+
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex == FINAL_LEVEL_ID)
+        {
+            _finalLevel = true;
+            foreach (AudioSource a in _music) { a.Stop(); }
+            foreach (AudioSource a in _screams) { a.Stop(); }
+        }
     }
 
 
     void Update()
     {
-        if (_currentMusic == null || !_currentMusic.isPlaying)
+        if (!_finalLevel)
         {
-            if (_timeSinceMusic >= MUSIC_INTERMEDIATE_TIME)
+            if (_currentMusic == null || !_currentMusic.isPlaying)
             {
-                _timeSinceMusic = 0.0f;
-                _music[_rnd.Next() % _music.Length - 1].Play();
+                if (_timeSinceMusic >= MUSIC_INTERMEDIATE_TIME)
+                {
+                    _timeSinceMusic = 0.0f;
+                    _music[_rnd.Next() % _music.Length - 1].Play();
+                }
+                _timeSinceMusic += Time.deltaTime;
             }
-            _timeSinceMusic += Time.deltaTime;
-        }
 
-        if (_timeSinceScream >= SCREAMS_INTERMEDIATE_TIME)
-        {
-            _timeSinceScream = 0.0f;
-            _screams[_rnd.Next() % _screams.Length - 1].Play();
+            if (_timeSinceScream >= SCREAMS_INTERMEDIATE_TIME)
+            {
+                _timeSinceScream = 0.0f;
+                _screams[_rnd.Next() % _screams.Length - 1].Play();
+            }
+            _timeSinceScream += Time.deltaTime;
         }
-        _timeSinceScream += Time.deltaTime;
     }
 }
