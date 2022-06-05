5021c03b18d40cf28cb12a856073506bf4eda16b
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-06-04 21:49:53 -0700

    added chase music

--- Scripts/DuckChaseTrigger.cs
+++ Scripts/DuckChaseTrigger.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class DuckChaseTrigger : MonoBehaviour
 {
     [SerializeField] DuckNavigation _duck;
+    [SerializeField] AudioSource _music;
 
     void OnTriggerEnter(Collider other)
     {
         if (other.GetComponent<PlayerController>())
         {
+            _music.Play();
             _duck.SwitchState(DuckNavigation.NavState.FINAL_CHASE);
         }
     }
--- Scripts/Switch.cs
+++ Scripts/Switch.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class Switch : MonoBehaviour
 {
     [SerializeField] private DoorManager _door;
     [SerializeField] private Animator _animator;
     [SerializeField] private AudioSource _sound;
+    [SerializeField] private AudioSource _finalMusic;
 
     public void Interact()
     {
         _door.Open();
         _sound.Play();
         _animator.Play("Base Layer.Activate");
+
+        if (SceneManager.GetActiveScene().buildIndex == Ambience.FINAL_LEVEL_ID)
+        {
+            _finalMusic.Stop();
+        }
     }
 }
