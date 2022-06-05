7e74e15029ce8390a288d55cb7e198ac265a64ac
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-06-04 22:41:44 -0700

    removed debug statements

--- Scripts/DuckNavigation.cs
+++ Scripts/DuckNavigation.cs
@@ -199,7 +199,7 @@ public class DuckNavigation : MonoBehaviour
                 {
                     _stepTime = 0.0f;
                     var r = _rnd.Next() % (_stepSounds.Length - 1);
-                    Debug.Log(r);
+                    
                     _stepSounds[r].Play();
                 }
                 _stepTime += Time.deltaTime;
@@ -246,7 +246,7 @@ public class DuckNavigation : MonoBehaviour
                 {
                     _stepTime = 0.0f;
                     var r = _rnd.Next() % (_stepSounds.Length - 1);
-                    Debug.Log(r);
+                    
                     _stepSounds[r].Play();
                 }
                 _stepTime += Time.deltaTime;
@@ -270,7 +270,7 @@ public class DuckNavigation : MonoBehaviour
                 {
                     _stepTime = 0.0f;
                     var r = _rnd.Next() % (_stepSounds.Length - 1);
-                    Debug.Log(r);
+                    
                     _stepSounds[r].Play();
                 }
                 _stepTime += Time.deltaTime;
@@ -294,7 +294,7 @@ public class DuckNavigation : MonoBehaviour
                 {
                     _stepTime = 0.0f;
                     var r = _rnd.Next() % (_stepSounds.Length - 1);
-                    Debug.Log(r);
+                    
                     _stepSounds[r].Play();
                 }
                 _stepTime += Time.deltaTime;
@@ -325,7 +325,7 @@ public class DuckNavigation : MonoBehaviour
                 {
                     _stepTime = 0.0f;
                     var r = _rnd.Next() % (_stepSounds.Length - 1);
-                    Debug.Log(r);
+                    
                     _stepSounds[r].Play();
                 }
                 _stepTime += Time.deltaTime;
