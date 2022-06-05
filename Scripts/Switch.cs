947dac2b37bf37f5d5d5240696e0514a92d9e066
IAmNotIsaac <isaacjulianastell@gmail.com>
2022-06-04 20:25:07 -0700

    added lever sound

--- Scripts/Switch.cs
+++ Scripts/Switch.cs
@@ -7,11 +7,13 @@ public class Switch : MonoBehaviour
 {
     [SerializeField] private DoorManager _door;
     [SerializeField] private Animator _animator;
+    [SerializeField] private AudioSource _sound;
     public bool locked = false;
 
     public void Interact()
     {
         _door.Open();
+        _sound.Play();
         _animator.Play("Base Layer.Activate");
     }
 }
